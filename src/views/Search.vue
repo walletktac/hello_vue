40a8bf05b577b4b4e59eee82631e392480594fac
Karol <trzcinskikar@gmail.com>
2018-08-09 16:32:58 +0200

    connect app to NASA API

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,13 +3,19 @@
     <div class="search">
       <label for="search">Search</label>
       <input id="search" name="search" v-model="searchValue" @input="handleInput" />
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{ item.data[0].description }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-
-const API = 'https://images-api.nasa.gov'
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+const API = 'https://images-api.nasa.gov/search';
 
 
 export default {
@@ -17,12 +23,20 @@ export default {
   data() {
     return {
       searchValue: '',
+      results: [],
     };
   },
   methods: {
-    handleInput(e) {
-      console.log(this.searchValue);
-    },
+    // eslint-disable-next-line
+    handleInput: debounce(function() {
+        axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500) 
   },
 };
 </script>
