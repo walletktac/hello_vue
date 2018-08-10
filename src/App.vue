bf30d0c987e74f0958cbc84310bf2c7ce42a5718
Karol <trzcinskikar@gmail.com>
2018-08-10 18:29:11 +0200

    ready app

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
       <div class="results" v-if="results && !loading && step === 1">
         <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)"/>
       </div>
-      <Modal v-if="modalOpen" @closeModal="modalOpen = false" />
+      <div class="loader" v-if="step === 1 && loading" />
+      <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false" />
     </div>
 </template>
 
@@ -40,6 +41,7 @@ export default {
   data() {
     return {
       modalOpen: false,
+      modalItem: null,
       loading: false,
       step: 0,
       searchValue: "",
@@ -47,8 +49,9 @@ export default {
     };
   },
   methods: {
-    handleModalOpen() {
+    handleModalOpen(item) {
       this.modalOpen = true;
+      this.modalItem = item;
     },
     // eslint-disable-next-line
     handleInput: debounce(function() {
@@ -74,47 +77,85 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800");
 * {
   box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 body {
-  font-family: Montserrat, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   margin: 0;
   padding: 0;
 }
-.fade-enter-active, .fade-leave-active{
+.fade-enter-active, .fade-leave-active {
   transition: opacity .3s ease;
-} 
-.fade-enter, .fade-leave-to{
+}
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
-.slide-enter-active, .slide-leave-active{
+.slide-enter-active, .slide-leave-active {
   transition: margin-top .3s ease;
-} 
-.slide-enter, .slide-leave-to{
+}
+.slide-enter, .slide-leave-to {
   margin-top: -50px;
 }
 .wrapper {
+  margin: 0;
   position: relative;
+  width: 100%;
+  min-height: 100vh;
+  padding: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 0;
-  padding: 30px;
-  width: 100%;
-  min-height: 100vh;
   &.flexStart {
     justify-content: flex-start;
   }
 }
-
-.logo{
+.loader {
+  margin-top: 100px;
+  display: inline-block;
+  width: 64px;
+  height: 64px;
+  @media (min-width: 768px) {
+    width: 90px;
+    height: 90px;
+  }
+}
+.loader:after {
+  content: " ";
+  display: block;
+  width: 46px;
+  height: 46px;
+  margin: 1px;
+  border-radius: 50%;
+  border: 5px solid #1e3d4a;
+  border-color: #1e3d4a transparent #1e3d4a transparent;
+  animation: loading 1.2s linear infinite;
+  @media (min-width: 768px) {
+    width: 90px;
+    height: 90px;
+  }
+}
+@keyframes loading {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.logo {
   position: absolute;
   top: 30px;
 }
-.results{
-  margin-top:50px;
+.results {
+  margin-top: 50px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
+  @media (min-width: 768px) {
+    width: 90%;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 }
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,43 +1,37 @@
 <template>
-    
-        <div :style="style" class="item"/>
-   
+  <div :style="style" class="item" />
 </template>
 <script>
 export default {
   name: 'Item',
   props: {
     item: {
-        type: Object,
-        required: true,
+      type: Object,
+      required: true,
     },
   },
   data() {
-      return {
-          photo: this.item.links[0].href,
-          title: this.item.data[0].title,
+    return {
+      photo: this.item.links[0].href,
+      title: this.item.data[0].title,
     };
   },
   computed: {
-      style() {
-          return `background-image: url("${this.photo}")`;
-      }
-  }
+    style() {
+      return `background-image: url("${this.photo}")`;
+    },
+  },
 };
 </script>
-
 <style lang="scss" scoped>
-
-.item{
-    width:300px;
-    height: 300px;
+   .item {
+    width: 40vw;
+    height: 250px;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: 50%;
-}
-
+    @media (min-width: 768px) {
+      width: 25vw;
+    }
+  }
 </style>
-
-
-
-
